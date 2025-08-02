e0b3ca16798167c9d4796b7119ca260239075bd9
baovannguyen <baoquy1400@gmail.com>
2025-08-02 18:30:59 +0700

    deloy lần 8

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // ✅ Quan trọng để tải đúng tài nguyên trên Render
-
   plugins: [react()],
-
-  // ✅ Dùng để chạy thử build local bằng: npm run preview
-  preview: {
-    port: 4173,
-    host: true,
-    allowedHosts: ['datn-9f68.onrender.com'],
+  build: {
+    outDir: 'dist',
   },
-
-  // ✅ Chỉ dùng trong môi trường phát triển (localhost)
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:7003",
+        target: "https://localhost:7003", // hoặc URL backend online nếu đã deploy API
         changeOrigin: true,
         secure: false,
       },
     },
-  },
+  }
 });
