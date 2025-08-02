04a88d85dc7d88d5696afbd2ed89f7febb073ce9
baovannguyen <baoquy1400@gmail.com>
2025-08-02 18:13:30 +0700

    deloy lần 6

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    "/api": {
-      target: "https://localhost:7003",
-      changeOrigin: true,
-      secure: false,
+  // ✅ Thêm cấu hình preview
+  preview: {
+    port: 4173, // hoặc PORT mặc định nếu cần
+    host: true,
+    allowedHosts: ['datn-9f68.onrender.com'], // Cho phép host trên Render
+  },
+  // ✅ Cấu hình proxy dành cho dev
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://localhost:7003",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
